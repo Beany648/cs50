3e4ab3e17a30b66a4b4cfa3e7462f9038d20dc06
Beany648 <lol.chel3@mail.ru>
2022-01-30 16:41:07 +0300

    lalal

--- main.py
+++ main.py
@@ -6,3 +6,16 @@ name = ['main', productList,'milk']
 print (productList[0])
 print (name [1][0])
 print(name[1][1])
+def welcome():
+    print('fuck you')
+
+def height(m,cm):
+    total = (m*cm)+ cm
+    print(total, 'cm tall')
+height (1,70)
+
+def calc(a,b):
+    total = a+b
+    return total
+summ = calc (4,5)
+print(summ)
