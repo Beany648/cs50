25f16d6d6b3c6659f94181318d7270c6028f0ede
Beany648 <lol.chel3@mail.ru>
2022-01-30 18:43:06 +0300

    sss

--- def-liza.py
+++ def-liza.py
@@ -1,13 +1,7 @@
-from functools import total_ordering
 
-
-def lalala (f): 
+#def lalala (f): 
     total = 1+f
     if total>10:
         print ('fuck you')
     if total<10:
         print ('your mom is fucking looser')
-    else :
-        return & ag total
-lalala(9)
-
