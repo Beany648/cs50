3a2ba4f6ec5efe16d166db847c95b944ce954e7a
Beany648 <lol.chel3@mail.ru>
2022-02-20 14:37:45 +0300

    dick

--- def-liza.py
+++ def-liza.py
@@ -1,7 +1,6 @@
-
-#def lalala (f): 
-    total = 1+f
-    if total>10:
-        print ('fuck you')
-    if total<10:
-        print ('your mom is fucking looser')
+print ('Can i have some sex?')
+print ('What is size of your dick?')
+print ('you can check it yourself')
+dick = 20
+if (dick == 20) : print ("yes")
+else : print ("nothing")
